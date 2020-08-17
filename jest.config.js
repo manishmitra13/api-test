1733597ab87ee3074b6b82783424f6832191532f
User <user@192-168-1-119.tpgi.com.au>
2020-08-18 00:36:00 +1000

    Added HTMl Report

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,12 @@ module.exports = {
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
+  "reporters": [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "./html-report",
+      "filename": "report.html",
+      "expand": true
+    }]
+  ],
 }
